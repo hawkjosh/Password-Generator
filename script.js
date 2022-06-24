39d8c02a6d993c209232f0c1046b356044f69f65
Josh Hawk <hawkjosh@icloud.com>
2022-06-24 02:02:45 -0400

    Module 3 Assignment Push

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function generatePassword() {
   var userLength = prompt("How many characters would you like to use?  (Min=8  |  Max=128)");
   // Validate input to ensure length meets acceptance criteria
   if (userLength < 8 || userLength > 128) {
-    alert("Value must be a number between 8 and 128!");
-    generatePassword();
-    return;
+    return "😕ERROR: You must choose a number between 8 and 128.";
   }
   // Prompt user for password character type
   var userAlphaLo = confirm("Do you want to include lowercase letters?  (Okay=Yes  |  Cancel=No)");
@@ -18,55 +16,102 @@ function generatePassword() {
   var userSymbol = confirm("Do you want to include any special characters?  (Okay=Yes  |  Cancel=No)");
   // Validate input to ensure character type meets acceptance criteria
   if (!userAlphaLo && !userAlphaUp && !userNumber && !userSymbol) {
-    alert("ERROR: You must choose at least one type of character. Please try again.");
-    generatePassword();
-    return;
+    return "😕ERROR: You must choose at least one character type.";
   }
 
   // Generate password given user input
 
-  // Add lists to store all possible characters
+  // Define variables to store possible characters
   var listAlphaLo = "abcdefghijklmnopqrstuvwxyz"
   var listAlphaUp = listAlphaLo.toUpperCase()
-  // var listNumber = "0123456789"
-  // var listSymbol = "!@#$%^&*_-+="
+  var listNumber = "0123456789"
+  var listSymbol = "!@#$%^&*_-+="
   var listAlphaLoUp = listAlphaLo + listAlphaUp
-
-  var pwChars = ""
-  for (i=0; i < userLength; i++) {
-    if (userAlphaLo && !userAlphaUp) {
-      pwChars += listAlphaLo.charAt(Math.floor(Math.random() * listAlphaLo.length));
-    }
-    else if (!userAlphaLo && userAlphaUp) {
-      pwChars += listAlphaUp.charAt(Math.floor(Math.random() * listAlphaUp.length));
-    }
-    else if (userAlphaLo && userAlphaUp) {
-      pwChars += listAlphaLoUp.charAt(Math.floor(Math.random() * listAlphaLoUp.length));
-    }
-  }
-
-  // for (i=0; i < userLength; i++) {
-  //   if (userAlphaUp) {
-  //     pwChars += listAlphaUp.charAt(Math.floor(Math.random() * listAlphaUp.length));
-  //   }
-  // }
-
-  // if (userNumber) {
-  //   pwChars += listNumber.charAt(Math.floor(Math.random() * listNumber.length));
-  // }
-
-  // if (userSymbol) {
-  //   pwChars += listSymbol.charAt(Math.floor(Math.random() * listSymbol.length));
-  // }
+  var listAlphaLoNum = listAlphaLo + listNumber
+  var listAlphaLoSym = listAlphaLo + listSymbol
+  var listAlphaUpNum = listAlphaUp + listNumber
+  var listAlphaUpSym = listAlphaUp + listSymbol
+  var listNumSym = listNumber + listSymbol
+  var listAlphaLoUpNum = listAlphaLo + listAlphaUp + listNumber
+  var listAlphaLoUpSym = listAlphaLo + listAlphaUp + listSymbol
+  var listAlphaLoNumSym = listAlphaLo + listNumber + listSymbol
+  var listAlphaUpNumSym = listAlphaUp + listNumber + listSymbol
+  var listAll = listAlphaLo + listAlphaUp + listNumber + listSymbol
 
   // Define variable to store randomly generated characters
-  var pwFinal = pwChars
-  // TODO: Remove code above and uncomment code below
-  // var finalPassword = possChar1 + possChar2 + possChar3 + possChar4
+  var pwChars = ""
+
+  // Generate password given user input
+  for (i=0; i < userLength; i++) {
+    // User input = lowercase only
+    if (userAlphaLo && !userAlphaUp && !userNumber & !userSymbol) {
+      pwChars += listAlphaLo.charAt(Math.floor(Math.random() * listAlphaLo.length));
+    }
+    // User input = uppercase only
+    else if (!userAlphaLo && userAlphaUp && !userNumber & !userSymbol) {
+      pwChars += listAlphaUp.charAt(Math.floor(Math.random() * listAlphaUp.length));
+    }
+    // User input = number only
+    else if (!userAlphaLo && !userAlphaUp && userNumber & !userSymbol) {
+      pwChars += listNumber.charAt(Math.floor(Math.random() * listNumber.length));
+    }
+    // User input = symbol only
+    else if (!userAlphaLo && !userAlphaUp && !userNumber & userSymbol) {
+      pwChars += listSymbol.charAt(Math.floor(Math.random() * listSymbol.length));
+    }
+    // User input = lowercase and uppercase
+    else if (userAlphaLo && userAlphaUp && !userNumber & !userSymbol) {
+      pwChars += listAlphaLoUp.charAt(Math.floor(Math.random() * listAlphaLoUp.length));
+    }
+    // User input = lowercase and number
+    else if (userAlphaLo && !userAlphaUp && userNumber & !userSymbol) {
+      pwChars += listAlphaLoNum.charAt(Math.floor(Math.random() * listAlphaLoNum.length));
+    }
+    // User input = lowercase and symbol
+    else if (userAlphaLo && !userAlphaUp && !userNumber & userSymbol) {
+      pwChars += listAlphaLoSym.charAt(Math.floor(Math.random() * listAlphaLoSym.length));
+    }
+    // User input = uppercase and number
+    else if (!userAlphaLo && userAlphaUp && userNumber & !userSymbol) {
+      pwChars += listAlphaUpNum.charAt(Math.floor(Math.random() * listAlphaUpNum.length));
+    }
+    // User input = uppercase and symbol
+    else if (!userAlphaLo && userAlphaUp && !userNumber & userSymbol) {
+      pwChars += listAlphaUpSym.charAt(Math.floor(Math.random() * listAlphaUpSym.length));
+    }
+    // User input = number and symbol
+    else if (!userAlphaLo && !userAlphaUp && userNumber & userSymbol) {
+      pwChars += listNumSym.charAt(Math.floor(Math.random() * listNumSym.length));
+    }
+    // User input = lowercase, uppercase, and number
+    else if (userAlphaLo && userAlphaUp && userNumber & !userSymbol) {
+      pwChars += listAlphaLoUpNum.charAt(Math.floor(Math.random() * listAlphaLoUpNum.length));
+    }
+    // User input = lowercase, uppercase, and symbol
+    else if (userAlphaLo && userAlphaUp && !userNumber & userSymbol) {
+      pwChars += listAlphaLoUpSym.charAt(Math.floor(Math.random() * listAlphaLoUpSym.length));
+    }
+
+    // User input = lowercase, number, and symbol
+    else if (userAlphaLo && !userAlphaUp && userNumber & userSymbol) {
+      pwChars += listAlphaLoNumSym.charAt(Math.floor(Math.random() * listAlphaLoNumSym.length));
+    }
+    // User input = uppercase, number, and symbol
+    else if (!userAlphaLo && userAlphaUp && userNumber & userSymbol) {
+      pwChars += listAlphaUpNumSym.charAt(Math.floor(Math.random() * listAlphaUpNumSym.length));
+    }
+    // User input = lowercase, uppercase, number, and symbol
+    else if (userAlphaLo && userAlphaUp && userNumber & userSymbol) {
+      pwChars += listAll.charAt(Math.floor(Math.random() * listAll.length));
+    }
+
+  }
 
   // Return the stored variable
-  return pwFinal
+  return pwChars;
+
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
