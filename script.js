c27fae82ec4bc83b3b6c6dfc71f04718056e02f3
Josh Hawk <hawkjosh@icloud.com>
2022-06-24 00:13:21 -0400

    Lower and upper case generation complete

--- script.js
+++ script.js
@@ -1,57 +1,72 @@
-// Assignment Code
+// Assignment CodeuserAlphaUp
 var generateBtn = document.querySelector("#generate");
 
 // Define function to generate password
 function generatePassword() {
   // Prompt user for password length
-  var pwLen = prompt("How many characters would you like to use?  (Min=8  |  Max=128)");
-  // Validate input to ensure length is between 8 and 128
-  if (pwLen < 8 || pwLen > 128) {
+  var userLength = prompt("How many characters would you like to use?  (Min=8  |  Max=128)");
+  // Validate input to ensure length meets acceptance criteria
+  if (userLength < 8 || userLength > 128) {
     alert("Value must be a number between 8 and 128!");
     generatePassword();
     return;
   }
   // Prompt user for password character type
-  var pwLowCase = confirm("Do you want to include lowercase letters?  (Okay=Yes  |  Cancel=No)");
-  var pwUpCase = confirm("Do you want to include uppercase letters?  (Okay=Yes  |  Cancel=No)");
-  var pwNum = confirm("Do you want to include any numeric values?  (Okay=Yes  |  Cancel=No)");
-  var pwSpec = confirm("Do you want to include any special characters?  (Okay=Yes  |  Cancel=No)");
-  // Validate input to ensure at least one character type selected
-  if (!pwLowCase && !pwUpCase && !pwNum && !pwSpec) {
+  var userAlphaLo = confirm("Do you want to include lowercase letters?  (Okay=Yes  |  Cancel=No)");
+  var userAlphaUp = confirm("Do you want to include uppercase letters?  (Okay=Yes  |  Cancel=No)");
+  var userNumber = confirm("Do you want to include any numeric values?  (Okay=Yes  |  Cancel=No)");
+  var userSymbol = confirm("Do you want to include any special characters?  (Okay=Yes  |  Cancel=No)");
+  // Validate input to ensure character type meets acceptance criteria
+  if (!userAlphaLo && !userAlphaUp && !userNumber && !userSymbol) {
     alert("ERROR: You must choose at least one type of character. Please try again.");
     generatePassword();
     return;
   }
 
-// Generate password given user input
+  // Generate password given user input
 
-// TODO: Add a list of all possible characters given user input
-var listLowCase = "abcdefghijklmnopqrstuvwxyz"
-var listUpCase = lowCaseChar.toUpperCase()
-var listNum = "0123456789"
-var listSpec = "!#$%&*+,-./:;<=>?@^_`|~"
+  // Add lists to store all possible characters
+  var listAlphaLo = "abcdefghijklmnopqrstuvwxyz"
+  var listAlphaUp = listAlphaLo.toUpperCase()
+  // var listNumber = "0123456789"
+  // var listSymbol = "!@#$%^&*_-+="
+  var listAlphaLoUp = listAlphaLo + listAlphaUp
 
-var possChar = ""
+  var pwChars = ""
+  for (i=0; i < userLength; i++) {
+    if (userAlphaLo && !userAlphaUp) {
+      pwChars += listAlphaLo.charAt(Math.floor(Math.random() * listAlphaLo.length));
+    }
+    else if (!userAlphaLo && userAlphaUp) {
+      pwChars += listAlphaUp.charAt(Math.floor(Math.random() * listAlphaUp.length));
+    }
+    else if (userAlphaLo && userAlphaUp) {
+      pwChars += listAlphaLoUp.charAt(Math.floor(Math.random() * listAlphaLoUp.length));
+    }
+  }
 
-if (pwLowCase) {
-  possChar += listLowCase;
+  // for (i=0; i < userLength; i++) {
+  //   if (userAlphaUp) {
+  //     pwChars += listAlphaUp.charAt(Math.floor(Math.random() * listAlphaUp.length));
+  //   }
+  // }
+
+  // if (userNumber) {
+  //   pwChars += listNumber.charAt(Math.floor(Math.random() * listNumber.length));
+  // }
+
+  // if (userSymbol) {
+  //   pwChars += listSymbol.charAt(Math.floor(Math.random() * listSymbol.length));
+  // }
+
+  // Define variable to store randomly generated characters
+  var pwFinal = pwChars
+  // TODO: Remove code above and uncomment code below
+  // var finalPassword = possChar1 + possChar2 + possChar3 + possChar4
+
+  // Return the stored variable
+  return pwFinal
 }
-if (pwUpCase) {
-  possChar += listUpCase;
-}
-if (pwNum) {
-  possChar += listNum;
-}
-if (pwSpec) {
-  possChar += listSpec;
-}
-
-// TUTOR SESSION NOTES → Use a for loop here with my list, will need to apply math.floor && math.random
-
-// TODO: Define variable to store chosen characters
-
-// Return the stored variable
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
